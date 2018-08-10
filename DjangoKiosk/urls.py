f1cccaf12735aebfeeca17a84cb46249c8178228
san90279 <san90279@gmail.com>
2018-08-10 13:37:12 +0800

    Finish EmployeeCard V1

--- DjangoKiosk/urls.py
+++ DjangoKiosk/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.conf.urls import url,include
+from django.conf.urls import url
+from django.urls import path,include
 from polls.views import home, login
 from Auth.views import V_Login, V_CheckAuth
-from EmployeeCard.views import V_EmployeeCardIndex
+from EmployeeCard.views import V_EmployeeCardIndex,V_GetEmployeeCardData,V_EmployeeCardEdit,V_EmployeeCardSave
 
 Auth_urlpatterns = [
     url(r'^login/$', V_Login),
@@ -10,7 +11,10 @@ Auth_urlpatterns = [
 ]
 
 EmployeeCard_urlpatterns = [
-    url(r'^index/$', V_EmployeeCardIndex),
+    path('index/', V_EmployeeCardIndex),
+    path('GetEmployeeCardData/', V_GetEmployeeCardData),
+    path('Edit/<int:id>/', V_EmployeeCardEdit),
+    path('Save/', V_EmployeeCardSave),
 ]
 
 urlpatterns = [
@@ -19,5 +23,5 @@ urlpatterns = [
     url(r'^login/$',login),
     url(r'^polls/', include('polls.urls')),
     url(r'^Auth/', include(Auth_urlpatterns)),
-    url(r'^EmployeeCard/', include(EmployeeCard_urlpatterns)),
+    path('EmployeeCard/', include(EmployeeCard_urlpatterns)),
 ]
--- EmployeeCard/views.py
+++ EmployeeCard/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from EmployeeCard.models import M_EmployeeCard
+from django.core import serializers
+import datetime
 
 # Create your views here.
 def V_EmployeeCardIndex(request):
     return render(request,'EmployeeCard/index.html');
+
+
+def V_GetEmployeeCardData(request):
+    try:
+        EmployeeCardData=M_EmployeeCard.objects.all()
+    except:
+        EmployeeCardData=None
+    EmployeeCardDataJson = serializers.serialize('json', EmployeeCardData)
+    return HttpResponse(EmployeeCardDataJson, content_type='application/json')
+
+
+def V_EmployeeCardEdit(request,id):
+    if(id==0):
+        EmployeeCardData=None
+    else:
+        EmployeeCardData=M_EmployeeCard.objects.get(pk=id).__dict__
+    return render(request,'EmployeeCard/Edit.html',EmployeeCardData);
+
+
+def V_EmployeeCardSave(request):
+    EmployeeID = request.POST.get('EmployeeID', '')
+    EmployeeName = request.POST.get('EmployeeName', '')
+    CardNo = request.POST.get('CardNo', '')
+    IsWork = request.POST.get('IsWork', 'False')
+    pk = request.POST.get('pk', '')
+
+    if(pk!=''):
+        EmployeeCardData=M_EmployeeCard.objects.get(pk=pk)
+    else:
+        EmployeeCardData=M_EmployeeCard()
+    EmployeeCardData.EmployeeID=EmployeeID
+    EmployeeCardData.EmployeeName=EmployeeName
+    EmployeeCardData.CardNo=CardNo
+    EmployeeCardData.IsWork=IsWork
+    EmployeeCardData.Editor=request.user
+    EmployeeCardData.EditDate=datetime.datetime.now()
+    EmployeeCardData.save()
+    return render(request,'EmployeeCard/index.html')
