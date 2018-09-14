f9c00b46552c63d990c3b0ffc4fa7b4dc9b84c69
a100222213 <u29446142@gmail.com>
2018-09-14 15:47:19 +0800

    dropdown_helper_V2

--- CommonApp/templatetags/Common_extras.py
+++ CommonApp/templatetags/Common_extras.py
@@ -3,17 +3,25 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 register = template.Library()
 
-@register.filter()
-def ParameterListFor(list,args, autoescape=True):
-    if args is None:
-        return ''
-    arg_list = [arg.strip() for arg in args.split(',')]
-    selecthelper='<select class="form-control rounded-0 col-xl-3 col-sm-6 mb-3" id="%s" name="%s">' % (arg_list[0],arg_list[0])
-    try:
-        selecthelper='%s<option value="">%s</option>' % (selecthelper,arg_list[1])
-    except Exception as e:
-        pass
+@register.simple_tag
+def ParameterListFor(*args, **kwargs):
+    list = defaultnull(kwargs,'list')
+    html_id =  defaultnull(kwargs,'html_id')
+    pleaseselect=  defaultnull(kwargs,'pleaseselect')
+    selectitem=  defaultnull(kwargs,'selectitem')
+
+    selecthelper='<select class="form-control rounded-0 col-xl-3 col-sm-6 mb-3" id="%s" name="%s">' % (html_id,html_id)
+    if pleaseselect != '':
+        selecthelper='%s<option value="">%s</option>' % (selecthelper,pleaseselect)
 
     for key,val in list:
-        selecthelper='%s<option value=%s>%s</option>' % (selecthelper,key,val)
+        if pleaseselect == '' and selectitem==key:
+            selecthelper='%s<option value=%s selected="selected">%s</option>' % (selecthelper,key,val)
+        else:
+            selecthelper='%s<option value=%s>%s</option>' % (selecthelper,key,val)
     return mark_safe(selecthelper+'</select>')
+def defaultnull(kwargs,key):
+    try:
+        return kwargs[key]
+    except:
+        return ''
