acccfb4b04c174100daf9cb2b6c1d5f255aaa000
san90279 <san90279@gmail.com>
2018-09-18 20:54:21 +0800

    Document Update

---
+++ 戶政找收系統文件/文件/舊資料/有使用的資料表.sql
@@ -0,0 +1,36 @@
+select * from ECRSBPC
+select * from REGGDSAUD
+select * from RPTSBM_TMP
+select * from REGGDSNBAK
+select * from ECRPYHS
+select * from ECRSBC
+select * from ECRTRHS
+select * from ECRSBD
+select * from ECRSBG
+select * from ECRSBU
+select * from CHGBKDET
+select * from REGREMHS
+select * from MainMenu
+select * from REGXXHS_RPT
+select * from UserLevel
+select * from ECRCASHR
+select * from ECRHDHS
+select * from EMPLOYEE_Log
+select * from RptSBM
+select * from RptSBD
+select * from REGXXHS
+
+
+
+--°òÂ¦¸ê®Æ
+select * from GCLASS4
+select * from GOODSLST
+select * from APMenuList
+select * from REGGDSN
+select * from FineType
+select * from article
+select * from ECRCFG
+select * from ReportMenu
+
+
+select * from ADDRCITY
