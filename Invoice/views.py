8b2e1c4c87b6f8970ed7b5c32d22d17c1d852f47
san90279 <san90279@gmail.com>
2018-09-28 09:30:08 +0800

    fixed bug in html

--- Invoice/views.py
+++ Invoice/views.py
@@ -12,7 +12,10 @@ from Invoice.forms import AddInvoiceForm
 from django.db.models import Max
 # Create your views here.
 def V_InvoiceIndex(request):
-    return render(request,'Invoice/index.html')
+    Status=M_Invoice.InvoiceStatusList
+    FeeID=M_FeeItem.objects.all().values_list('FeeID', 'FeeName')
+    Station=M_Station.objects.all().values_list('StationID', 'StationName')
+    return render(request,'Invoice/index.html',{'FeeID':FeeID,'Status':Status,'Station':Station})
 
 
 @csrf_protect
